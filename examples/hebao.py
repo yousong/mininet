fb54203fa6f298ed764356c9e6ce1c6acffe4bb2
Yousong Zhou <zhouyousong@yunionyun.com>
2018-10-31 04:13:35 +0000

    routes

--- examples/hebao.py
+++ examples/hebao.py
@@ -28,14 +28,19 @@ executing 'ip route' or 'route' commands on the router or hosts.
 """
 
 
+import json
+
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
+from mininet.link import Intf
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
+class LinuxHost(Node):
+    pass
 
-class LinuxRouter( Node ):
+class LinuxRouter(LinuxHost):
     "A Node with IP forwarding enabled."
 
     def config( self, **params ):
@@ -47,16 +52,29 @@ class LinuxRouter( Node ):
         self.cmd( 'sysctl net.ipv4.ip_forward=0' )
         super( LinuxRouter, self ).terminate()
 
-class NatRouter( LinuxRouter ):
+class RoutesIntf(Intf):
 
-    def config( self, **params ):
-        super( NatRouter, self).config( **params )
-        # Enable forwarding on the router
-        self.cmd( 'iptables -t nat -A POSTROUTING -' )
+    def config(self, **params):
+        r = super(RoutesIntf, self).config(**params)
+        self.setParam(r, 'setRoutes', routes=params.get('routes'))
+        self.setParam(r, 'setMasquerade', routes=params.get('masquerade'))
+        return r
 
-    def terminate( self ):
-        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
-        super( NatRouter, self ).terminate()
+    def setRoutes(self, masquerade):
+        if masquerade:
+            return self.cmd('iptables -t nat -A POSTROUTING -o %s -j MASQUERADE' % self.name)
+
+    def setRoutes(self, *routes):
+        result = ''
+        for route in routes:
+            net = route[0]
+            cmd = 'ip route add ' + net
+            if len(route) >= 2:
+                gw = route[1]
+                cmd += ' via ' + gw
+            cmd += ' dev ' + self.name
+            result += self.cmd(cmd)
+        return result
 
 class NetworkTopo( Topo ):
     "A LinuxRouter connecting three IP subnets"
@@ -68,27 +86,28 @@ class NetworkTopo( Topo ):
                 'office_switch': {},
                 'office_vm_switch': {},
                 'unicom_switch': {},
+                'openvpn_switch': {},
             },
             'hosts': {
                 'office_air': {
                     'ip': '192.168.122.107/24',
                     'switch': 'office_switch',
                     'routes': [
-                        ('0.0.0.0/24', '192.168.222.1'),
+                        ('0.0.0.0/0', '192.168.122.1'),
                     ],
                 },
                 'office_printer': {
                     'ip': '192.168.122.127/24',
                     'switch': 'office_switch',
                     'routes': [
-                        ('0.0.0.0/24', '192.168.222.1'),
+                        ('0.0.0.0/0', '192.168.122.1'),
                     ],
                 },
                 'office_vm_titan': {
                     'ip': '10.168.222.136/24',
                     'switch': 'office_vm_switch',
                     'routes': [
-                        ('0.0.0.0/24', '10.168.222.1'),
+                        ('0.0.0.0/0', '10.168.222.1'),
                     ],
                 },
             },
@@ -101,11 +120,15 @@ class NetworkTopo( Topo ):
                     'wan': {
                         'ip': '192.168.222.100/24',
                         'switch': 'unicom_switch',
-                        'nat': True,
+                        'masquerade': True,
                         'routes': [
-                            ('0.0.0.0/24', '192.168.222.1'),
+                            ('0.0.0.0/0', '192.168.222.1'),
                             ('10.168.222.0/24', '192.168.222.90'),
                         ],
+                    },
+                    'openvpn': {
+                        'ip': '10.8.0.2/24',
+                        'switch': 'openvpn_switch',
                     },
                 },
                 'kubenode': {
@@ -113,7 +136,14 @@ class NetworkTopo( Topo ):
                         'ip': '192.168.222.171/24',
                         'switch': 'unicom_switch',
                         'routes': [
-                            ('0.0.0.0/24', '192.168.222.1')
+                            ('0.0.0.0/0', '192.168.222.1')
+                        ],
+                    },
+                    'openvpn': {
+                        'ip': '10.8.0.31/24',
+                        'switch': 'openvpn_switch',
+                        'routes': [
+                            ('192.168.122.0/24', '10.8.0.2'),
                         ],
                     },
                 },
@@ -121,12 +151,18 @@ class NetworkTopo( Topo ):
                     'host': {
                         'ip': '192.168.222.90/24',
                         'switch': 'unicom_switch', #
-                        'nat': True,
+                        'masquerade': True,
                     },
                     'vm': {
                         'ip': '10.168.222.1/24',
                         'switch': 'office_vm_switch',
-                    }
+                    },
+                },
+                'routervpn': {
+                    'vpn': {
+                        'ip': '10.8.0.1/24',
+                        'switch': 'openvpn_switch', #
+                    },
                 },
             },
         }
@@ -134,13 +170,13 @@ class NetworkTopo( Topo ):
         for name, sw in topo['switches'].iteritems():
             sw['_name'] = 's%d' % swi
             swi += 1
-            self.addSwitch(sw['_name'])
+            self.addSwitch(sw['_name'], failMode='standalone')
         hi = 0
         for name, h in topo['hosts'].iteritems():
             h['_name'] = 'h%d' % hi
             hi += 1
             self.addHost(h['_name'], ip=None)
-            self.addLink(h['_name'], topo['switches'][h['switch']]['_name'], params1={'ip': h['ip']})
+            self.addLink(h['_name'], topo['switches'][h['switch']]['_name'], params1={'ip': h['ip'], 'routes': h.get('routes')})
         ri = 0
         for name, r in topo['routers'].iteritems():
             r['_name'] = 'r%d' % ri
@@ -149,54 +185,27 @@ class NetworkTopo( Topo ):
             for portname, port in r.iteritems():
                 if portname.startswith('_'):
                     continue
-                self.addLink(r['_name'], topo['switches'][port['switch']]['_name'], params1={'ip': port['ip']})
+                params1 = {
+                    'ip': port['ip'],
+                    'routes': port.get('routes'),
+                    'masquerade': port.get('masquerade'),
+                }
+                self.addLink(r['_name'], topo['switches'][port['switch']]['_name'], params1=params1)
+
+        with open('devenv.json', 'w') as fout:
+            fout.write(json.dumps(topo, indent=2))
+
 
         return
-        # r0, s0: office router
-        r0_ip_lan = '192.168.122.1/24'
-        r0_ip_wan = '192.168.222.100/24'
-        r0 = self.addNode( 'r0', cls=LinuxRouter, ip=r0_ip_lan )
-        s0 = self.addSwitch( 's0' )
-        self.addLink( s0, r0, params2={ 'ip': r0_ip_lan } )
-
-        # r1, s1: unicom isp router
-        r1_ip_lan = '192.168.222.1/24'
-        r1 = self.addNode( 'r1', cls=LinuxRouter, ip=r1_ip_lan )
-        s1 = self.addSwitch( 's1' )
-        self.addLink( s1, r1, params2={ 'ip': r1_ip_lan } )
-        self.addLink( s1, r0, params2={ 'ip': r0_ip_wan } )
-
-        # h0: office notebook
-        # h1: office printer
-        h0 = self.addHost( 'h0', ip='192.168.122.107/24', defaultRoute='via 192.168.122.1' )
-        h1 = self.addHost( 'h1', ip='192.168.122.127/24', defaultRoute='via 192.168.122.1' )
-        self.addLink( h0, s0 )
-        self.addLink( h1, s0 )
-
-        # r2, s2: kubenode
-        r2_ip_host = '192.168.222.171/24'
-        r2_ip_vpn = '10.8.0.31/24'
-        r2 = self.addNode( 'r2', cls=LinuxRouter, ip=r2_ip_host )
-        s2 = self.addSwitch( 's2' )
-        self.addLink( s2, r2, params2={ 'ip': r2_ip_host } )
-        self.addLink( s1, r2 )
-
-        # r3, s3: router222
-        r3_ip_ext = '192.168.222.90/24'
-        r3_ip_vm = '10.168.222.0/24'
-        r3 = self.addNode( 'r3', cls=LinuxRouter, ip=r3_ip_ext )
-        s3 = self.addSwitch( 's3' )
-        self.addLink( s3, r3, params2={ 'ip': r3_ip_ext } )
-        self.addLink( s2, r3 )
 
 def run():
     "Test linux router"
     topo = NetworkTopo()
-    net = Mininet( topo=topo )  # controller is used by s1-s3
+    net = Mininet( topo=topo, intf=RoutesIntf )
     net.start()
     info( '*** Routing Table on Router:\n' )
-    info( net[ 'r0' ].cmd( 'route' ) )
-    info( net[ 'r1' ].cmd( 'route' ) )
+    info( net[ 'r0' ].cmd( 'route -n' ) )
+    info( net[ 'r1' ].cmd( 'route -n' ) )
     CLI( net )
     net.stop()
 
